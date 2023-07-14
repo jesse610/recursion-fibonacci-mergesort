c18b6cd11ff6d7958ad2cb635782904977f10dd0
Jesse <jvelas028@ucr.edu>
2023-07-14 12:23:16 -0700

    adjusted readability and optimized processes

--- merge sort/mergesort.js
+++ merge sort/mergesort.js
@@ -1,41 +1,37 @@
 const mergeSort = (arr) => {
-    if (arr.length == 1)
+    if (arr.length === 1)
     {
       return [arr[0]]
     }
-    else if (arr.length == 2)
+    else if (arr.length === 2)
     {
       return arr[0] > arr[1] ? [arr[1], arr[0]] : [arr[0], arr[1]]
     }
   
+    const middle = Math.floor(arr.length / 2)
+    const left = mergeSort(arr.slice(0, middle))
+    const right = mergeSort(arr.slice(middle))
+
     let sortedArr = []
-    let left = mergeSort(arr.slice(0, arr.length / 2))
-    let right = mergeSort(arr.slice(arr.length / 2))
+    let leftIndex = 0
+    let rightIndex = 0
   
-    while(left.length != 0 || right.length != 0)
-    {
-      if (left[0] > right[0])
-      {
-        sortedArr.push(right[0])
-        right.splice(0, 1)
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] <= right[rightIndex]) {
+          sortedArr.push(left[leftIndex]);
+          leftIndex++;
+        } else {
+          sortedArr.push(right[rightIndex]);
+          rightIndex++;
+        }
       }
-      else if (left[0] < right[0])
-      {
-        sortedArr.push(left[0])
-        left.splice(0, 1)
-      }
-      
-      if (right.length == 0)
-      {
-        sortedArr.push(...left)
-        left = []
-      }
-      else if (left.length == 0) 
-      {
-        sortedArr.push(...right)
-        right = []
-      }
-    }
-      return sortedArr
-  }
+    
+      // Append remaining elements from either left or right array
+      sortedArr.push(...left.slice(leftIndex));
+      sortedArr.push(...right.slice(rightIndex));
+    
+      return sortedArr;
+}
+
+console.log(mergeSort([1, 6, 2, 2, 10, 4, 3, 53, 32]))
   
